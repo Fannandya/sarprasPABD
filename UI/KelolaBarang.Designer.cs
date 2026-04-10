8ade6d583935d52909348a5ca5e7c21a8d5ba3d9
Fannandya Sutan <sfannandya@gmail.com>
2026-04-10 18:03:15 +0700

    menambahkan form input data barang

---
+++ UI/KelolaBarang.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace projekPABD1
+{
+    partial class KelolaBarang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKembali = new System.Windows.Forms.Button();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.txtKategori = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.showData = new System.Windows.Forms.DataGridView();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.showData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKembali
+            // 
+            this.btnKembali.Location = new System.Drawing.Point(44, 393);
+            this.btnKembali.Name = "btnKembali";
+            this.btnKembali.Size = new System.Drawing.Size(111, 33);
+            this.btnKembali.TabIndex = 0;
+            this.btnKembali.Text = "kembali";
+            this.btnKembali.UseVisualStyleBackColor = true;
+            this.btnKembali.Click += new System.EventHandler(this.btnKembali_Click);
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(44, 65);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(215, 22);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtStok
+            // 
+            this.txtStok.Location = new System.Drawing.Point(44, 133);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(215, 22);
+            this.txtStok.TabIndex = 2;
+            // 
+            // txtKategori
+            // 
+            this.txtKategori.Location = new System.Drawing.Point(44, 214);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.Size = new System.Drawing.Size(215, 22);
+            this.txtKategori.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nama Barang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Stok";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kategori";
+            // 
+            // showData
+            // 
+            this.showData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.showData.Location = new System.Drawing.Point(336, 46);
+            this.showData.Name = "showData";
+            this.showData.RowHeadersWidth = 51;
+            this.showData.RowTemplate.Height = 24;
+            this.showData.Size = new System.Drawing.Size(452, 380);
+            this.showData.TabIndex = 7;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(44, 265);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(105, 37);
+            this.btnSimpan.TabIndex = 8;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(154, 265);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(105, 37);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Hapus";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // KelolaBarang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.showData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKategori);
+            this.Controls.Add(this.txtStok);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.btnKembali);
+            this.Name = "KelolaBarang";
+            this.Text = "KelolaBarang";
+            ((System.ComponentModel.ISupportInitialize)(this.showData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKembali;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.TextBox txtKategori;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView showData;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
