2f4f4a8452790b25328af7ac5eef0667ef1b0e27
Fannandya Sutan <sfannandya@gmail.com>
2026-04-10 18:06:00 +0700

    menambahkan form di report

--- UI/Report.Designer.cs
+++ UI/Report.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.btnKembali = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCetak = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnKembali
@@ -41,14 +44,36 @@
             this.btnKembali.UseVisualStyleBackColor = true;
             this.btnKembali.Click += new System.EventHandler(this.btnKembali_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(720, 265);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnCetak
+            // 
+            this.btnCetak.Location = new System.Drawing.Point(41, 300);
+            this.btnCetak.Name = "btnCetak";
+            this.btnCetak.Size = new System.Drawing.Size(101, 36);
+            this.btnCetak.TabIndex = 2;
+            this.btnCetak.Text = "Cetak";
+            this.btnCetak.UseVisualStyleBackColor = true;
+            // 
             // Report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCetak);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnKembali);
             this.Name = "Report";
             this.Text = "Report";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +81,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnKembali;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCetak;
     }
 }
